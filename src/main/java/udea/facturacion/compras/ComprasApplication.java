091381f0b8a95c0b23c3ab63b1d88ae6741f5d3d
alejandro gomez hurtado <alejo.gomez.h@gmail.com>
2018-08-17 00:13:48 -0500

    Proyecto final diplomado microservicios nivel 1
    * Se configura para ser solo cliente rest

--- src/main/java/udea/facturacion/compras/ComprasApplication.java
+++ src/main/java/udea/facturacion/compras/ComprasApplication.java
@@ -1,12 +1,20 @@
 package udea.facturacion.compras;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComprasApplication {
+public class ComprasApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComprasApplication.class, args);
+		SpringApplication app = new SpringApplication(ComprasApplication.class);
+		app.setWebEnvironment(false);
+		app.run(args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
 	}
 }
